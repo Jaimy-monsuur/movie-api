f3129b2be2776e695cf252f701def13128b11cb8
Jaimy Monsuur <jaimymonsuur@gmail.com>
2023-04-06 17:39:57 +0200

    .

--- src/middlewares/cors.middleware.go
+++ src/middlewares/cors.middleware.go
@@ -4,10 +4,11 @@ import "github.com/gin-gonic/gin"
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true") // Add this line to enable CORS requests with credentials
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
