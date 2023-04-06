61dba3f5fe79f384848f33b6aad8f57555886216
Jaimy Monsuur <jaimymonsuur@gmail.com>
2023-04-06 13:49:23 +0200

    fix i hope

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jaimy-monsuur/movie-api/src/config"
 	_ "github.com/jaimy-monsuur/movie-api/src/docs"
+	"github.com/jaimy-monsuur/movie-api/src/middlewares"
 	"github.com/jaimy-monsuur/movie-api/src/routes"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -17,6 +18,8 @@ func init() {
 func main() {
 
 	router := gin.Default()
+
+	router.Use(middlewares.CorsMiddleware())
 
 	routes.IndexRoutes(router)
 
