ca1cc59dff2a9d96f980ad9da139df925fc13e58
Jaimy Monsuur <jaimymonsuur@gmail.com>
2023-04-06 16:54:22 +0200

    .

--- src/middlewares/cors.middleware.go
+++ src/middlewares/cors.middleware.go
@@ -7,7 +7,6 @@ func CorsMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
